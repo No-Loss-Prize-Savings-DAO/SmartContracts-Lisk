51eb9e1fb12ae7bcca53561446e3e605266afcf1
Ekarika Nsemeke <hamatthan@gmail.com>
2024-04-27 20:04:19 +0100

    deployed to the ethereum sepolia

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,15 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+ solidity: "0.8.20",
+ networks: {
+    sepolia: {
+      url: process.env.URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+ },
 };
 
 export default config;
---
+++ ignition/modules/SavingsContract.ts
@@ -0,0 +1,30 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+// const JAN_1ST_2030 = 1893456000;
+// const ONE_GWEI: bigint = 1_000_000_000n;
+const STABLE_TOKEN_NAME = "USD Tether";
+const STABLE_TOKEN_SYMBOL = "USDT";
+const CONTRACT_TOKEN_NAME = "Blitz";
+const CONTRACT_TOKEN_SYMBOL = "BLZ";
+const stableCoinDecimals = 6;
+const contractTokenDecimals = 18;
+
+const SavingsContractModule = buildModule("SavingsContractModule", (m) => {
+  // const unlockTime = m.getParameter("unlockTime", JAN_1ST_2030);
+  // const lockedAmount = m.getParameter("lockedAmount", ONE_GWEI);
+
+  const stableToken = m.contract("USDT", [STABLE_TOKEN_NAME, STABLE_TOKEN_SYMBOL, stableCoinDecimals]);
+  const contractToken = m.contract("BLZ", [CONTRACT_TOKEN_NAME, CONTRACT_TOKEN_SYMBOL, contractTokenDecimals]);
+
+  const complianceDatabase = m.contract("ComplianceDatabase");
+  const regulatoryCompliance = m.contract("RegulatoryCompliance", [complianceDatabase]);
+  const nftContract = m.contract("NFTContract");
+
+  const savingsContract = m.contract("SavingsContract", [stableToken, contractToken, regulatoryCompliance, nftContract]);
+  const daoGovernance = m.contract("DAOGovernance", [savingsContract]);
+  const prizeDistribution = m.contract("PrizeDistribution", [9806, savingsContract, daoGovernance]);
+
+  return { stableToken, contractToken, complianceDatabase, regulatoryCompliance, nftContract, savingsContract, daoGovernance, prizeDistribution};
+});
+
+export default SavingsContractModule;
